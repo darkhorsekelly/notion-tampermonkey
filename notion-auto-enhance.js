72ca9a853ed4361bb94a81357fb80f513b9cc246
andrewwkelly <aw@andrewkelly.io>
2025-09-26 14:30:05 -0400

    Version control

--- notion-auto-enhance.js
+++ notion-auto-enhance.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Notion Tiny H1 + Auto Details
 // @namespace    http://tampermonkey.net/
-// @version      2.1.2
+// @version      2.1.3
 // @description  Reduces H1 size, hides controls, opens details pane, adds section colors, decreases vertical height, swap page content order
 // @author       You
 // @match        https://www.notion.so/*
